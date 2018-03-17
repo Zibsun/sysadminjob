8bf44dce65c71774c0846a9ad38a1e13bed8ceee
Denis <ms-ms@yandex.ru>
2018-03-17 11:02:53 +0300

    db + crowler

--- crowler/crowler.py
+++ crowler/crowler.py
@@ -1,9 +1,46 @@
+from db import db_session, Hh_vacancy, Vacancy
 import json
-import requests 
+import requests
 
-text = 'системный администратор Москва'
-link = 'https://api.hh.ru/vacancies?text=%s' % text
-r = requests.get(link, headers={'User-Agent': 'api-test-agent'})
+h = Hh_vacancy
+null = None
+text = 'Системный+администратор'
+link = 'https://api.hh.ru/vacancies?text={0}&area=1&no_magic=true&per_page=100'.format(text)
+key = ''
+au = 'Bearer ' + key
+
+r = requests.get(link, headers={'User-Agent': 'api-test-agent', 'Authorization': au})
 data = r.json()
-with open('response.json', 'w', encoding='utf-8') as file:
-    json.dump(data, file, indent=2, ensure_ascii=False)
+print(data['pages'])
+print(data['found'])
+pages = data['pages'] + 1
+
+for page in range(pages):
+    link = 'https://api.hh.ru/vacancies?text={1}&area=1&page={0}&per_page=100'.format(page, text)
+    print(page)
+    r = requests.get(link, headers={'User-Agent': 'api-test-agent'})
+    data = r.json()
+    with open('response.json', 'a', encoding='utf-8') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+    for vacancy in data['items']:
+        try:
+            hh_vacancy = Hh_vacancy(vacancy['salary']['from'], vacancy['salary']['to'], vacancy['salary']['currency'], vacancy['salary']['gross'], vacancy['name'], vacancy['area']['name'], vacancy['snippet']['responsibility'], vacancy['snippet']['requirement'],vacancy.setdefault('employer').setdefault('id'), vacancy['employer']['name'])
+        except TypeError:
+            print('Не указана зарплата')
+            hh_vacancy = Hh_vacancy(None, None, None, None, vacancy['name'], vacancy['area']['name'], vacancy['snippet']['responsibility'], vacancy['snippet']['requirement'], vacancy['id'], vacancy.setdefault('employer').setdefault('id'), vacancy['employer']['name'])
+        db_session.add(hh_vacancy)
+        db_session.commit()
+        hh_id = h.query.filter(Hh_vacancy.vacancy_id == vacancy['id']).first()
+        url = vacancy['url']
+        r = requests.get(url, headers={'User-Agent': 'api-test-agent'})
+        data = r.json()
+        try:
+            vacancy1 = Vacancy(data['salary']['from'], data['salary']['to'], data['name'], data['description'], str(data['key_skills']), data['experience']['name'], hh_id.id)
+        except TypeError:
+            print('Не указана зарплата')
+            vacancy1 = Vacancy(None, None, data['name'], data['description'], str(data.setdefault('key_skills')), data.setdefault('experience').setdefault('name'), hh_id.id)
+        db_session.add(vacancy1)
+        db_session.commit()
+        with open('response_vacancy.json', 'a', encoding='utf-8') as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
+
---
+++ crowler/db.py
@@ -0,0 +1,121 @@
+from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///mybase.db')
+
+db_session = scoped_session(sessionmaker(bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+class Hh_vacancy(Base):
+    __tablename__ = 'hh_vacancy'
+    id = Column(Integer, primary_key=True)
+    salary_from = Column(String(10))
+    salary_to = Column(String(10))
+    currency = Column(String(10))
+    gross = Column(String(10))
+    name = Column(String(20))
+    area_name = Column(String(10))
+    responsibility = Column(Text)
+    requirement = Column(Text)
+    vacancy_id = Column(Integer)
+    employer_name = Column(String(20))
+    employer_id = Column(String(20))
+    vacancy = relationship('Vacancy', backref='details')
+
+    def __init__(self, salary_from=None, salary_to=None, currency=None, gross=None, name=None, area_name=None, responsibility=None, requirement=None, vacancy_id=None, employer_id=None, employer_name=None):
+        self.salary_from = salary_from
+        self.salary_to = salary_to
+        self.currency = currency
+        self.gross = gross
+        self.name = name
+        self.area_name = area_name
+        self.responsibility = responsibility
+        self.requirement = requirement
+        self.employer_id = employer_id
+        self.employer_name = employer_name
+        self.vacancy_id = vacancy_id
+
+    def __repr__(self):
+        return '<User {} {}>'.format(self.employer_name, self.name)
+
+
+
+class Vacancy(Base):
+    __tablename__ = 'vacancy'
+    id = Column(Integer, primary_key=True)
+    salary_from = Column(String(10))
+    salary_to = Column(String(10))
+    name = Column(String(20))
+    responsibility = Column(Text)
+    requirement = Column(Text)
+    experience = Column(String(20))
+    hh_id = Column(Integer, ForeignKey('hh_vacancy.id'))
+
+    def __init__(self, salary_from=None, salary_to=None, name=None, responsibility=None, requirement=None, experience=None, hh_id=None):
+        self.salary_from = salary_from
+        self.salary_to = salary_to
+        self.name = name
+        self.responsibility = responsibility
+        self.requirement = requirement
+        self.experience = experience
+        self.hh_id = hh_id
+    def __repr__(self):
+        return '<User {} {}>'.format(self.name, self.salary_from)
+
+class Responsibility(Base):
+    __tablename__ = 'responsibility'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    description = Column(Text)
+
+    def __init__(self,  description=None,name=None):
+        self.description =  description
+        self.name = name
+
+    def __repr__(self):
+        return '<User {} {}>'.format(self.name, self.description)
+
+class Requirement(Base):
+    __tablename__ = 'requirement'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    description = Column(Text)
+
+    def __init__(self,  description=None,name=None):
+        self.description =  description
+        self.name = name
+
+    def __repr__(self):
+        return '<User {} {}>'.format(self.name, self.description)
+
+class Vacancy_responsibility(Base):
+    __tablename__ = 'vacancy_responsibility'
+    vacancy_id = Column(Integer, ForeignKey('hh_vacancy.id'),  primary_key=True)
+    responsibility_id = Column(Integer, ForeignKey('responsibility.id'),  primary_key=True)
+
+
+class Vacancy_requirement(Base):
+    __tablename__ = 'vacancy_requirement'
+    vacancy_id = Column(Integer, ForeignKey('hh_vacancy.id'), primary_key=True)
+    requirement_id = Column(Integer, ForeignKey('requirement.id'),  primary_key=True)
+
+class Сompanies(Base):
+    __tablename__ = 'companies'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    description = Column(Text)
+
+    def __init__(self,  description=None,name=None):
+        self.description =  description
+        self.name = name
+
+    def __repr__(self):
+        return '<User {} {}>'.format(self.name, self.description)
+
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
